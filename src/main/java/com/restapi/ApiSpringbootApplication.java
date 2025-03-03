23ac4cee2343563a369408733ed660003da3fbd5
Otis Lin <sp013719@gmail.com>
2025-03-04 00:24:51 +0800

    2nd commit to trigger github action

--- src/main/java/com/restapi/ApiSpringbootApplication.java
+++ src/main/java/com/restapi/ApiSpringbootApplication.java
@@ -11,7 +11,7 @@ public class ApiSpringbootApplication {
 
 	@GetMapping("/hello")
 	public String hello(){
-		return "springboot rest-api testing" ;
+		return "springboot rest-api testing 2" ;
 	}
 
 	public static void main(String[] args) {
