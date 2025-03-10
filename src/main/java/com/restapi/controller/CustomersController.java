a5fd9a6fccc08f17f8d704f490e45980e5f7c808
Otis Lin <sp013719@gmail.com>
2025-03-10 14:45:29 +0800

    fix db connection issue while running unit test

--- src/main/java/com/restapi/controller/CustomersController.java
+++ src/main/java/com/restapi/controller/CustomersController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/customers")
 public class CustomersController {
 
     private final RedisTemplate<String, Customer> redisTemplate;
--- src/test/java/com/restapi/ApiSpringbootApplicationTests.java
+++ src/test/java/com/restapi/ApiSpringbootApplicationTests.java
@@ -3,11 +3,11 @@ package com.restapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ApiSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
